e1ffa2da4b1858d9218e2d81584d14fef89b861f
Larry <tradesouthwest@outlook.com>
2020-07-07 13:23:00 -0700

    Update extended-functions.php
    simplified, other functions moved to templates

--- A-L/badgerplugcom/betheme-child/extended-functions.php
+++ A-L/badgerplugcom/betheme-child/extended-functions.php
@@ -1,60 +1,15 @@
 <?php 
 /**
  * Exclusions for quote cart button
- * @uses   $page_id Must equal numeric value of catalog post_type page
+ * @uses   no value - only to validate the function exists
  *
  * @see    https://developer.wordpress.org/reference/functions/is_page/
  * @author Larry @codeable
  */ 
 function betheme_child_request_aquote_echo()
 {   
-    global $post;
-    $parent_id        = $post->post_parent; 
-    $post_parent_slug = get_post_field( 'post_name', $parent_id );
-    $page_catalog = absint(3360);
-    $page_request = absint(5359);
-    // do nothing if
-    if ( is_home() || is_front_page() || is_page( array( 
-                                                    'products', 
-                                                    'roll-protection-products',
-                                                    'about',
-                                                    'resources',
-                                                    'services',
-                                                    'industries',
-                                                    'contact',
-                                                    'component-recycling-program',
-                                                    ) ) ) 
-    { 
-        echo '<a href="'. esc_url($action_link) .'" class="action_button'. esc_attr($action_class) .'" '. wp_kses_data($action_target) .'>'. wp_kses(mfn_opts_get('header-action-title'), mfn_allowed_html('button')) .'</a>';
-    } 
-    
-        // Get quotes count in (cart)
-        elseif( is_singular() || is_singular('single-product') ) 
-        { 
-        $url_icon = 'https://staging-badgerplugcom.kinsta.cloud/wp-content/uploads/2020/07/cart-icon_256x256.png';
-        $quote_cnt = '';
-        $quote_cnt = do_shortcode('[yith_ywraq_number_items class="qcnt" item_name="Item" item_plural_name="Items" show_url="no"]');
-        
-        // Clean and echo
-        ob_start();
-    
-        echo '<a class="action_button yith-items-button" href="' . home_url( '/' ) . 'request-quote/" title="quotes">
-    <span id="yithQuoteItem"><i class="iqcart"><img src="' . esc_url($url_icon) . '" height="14" width="16" alt=""/></i> 
-    <span id="quoteItemQnty"> &nbsp;' . $quote_cnt . ' </span> 
-    <span class="lnkspc"> &nbsp;' . __( " in Quote" ) . '</span></span></a>';
-    
-    $qbutton = ob_get_clean();
-        
-        echo $qbutton;
-
-        }
-        else
-        {
-    echo '<a href="'. esc_url($action_link) .'" class="action_button'. esc_attr($action_class) .'" '. wp_kses_data($action_target) .'>'. wp_kses(mfn_opts_get('header-action-title'), mfn_allowed_html('button')) .'</a>';
-    }
-    
+    return false;   
 }
-add_action( 'betheme_child_request_aquote_hook', 'betheme_child_request_aquote_echo' );
 
 /**
  * Get product attributes to display on yith-quote table
@@ -82,24 +37,3 @@ function betheme_child_get_product_terms_carton( $attribute_name, $product_id )
          return '';
      }
 } 
-/**
- * Register and parse custom product meta data
- * 
- * _carton_cnt_label cartons_pallet_label_ carton_cnt_value_ cartons_pallet_value_
- * post.php?post=5506&action=edit add to woocommerce_order_items
- */
-function betheme_child_register_ywraq_custom_attributes()
-{
-foreach ( $field['custom_attributes'] as $attribute => $value ) {
-			$custom_attributes[] = esc_attr( $attribute ) . '="' . esc_attr( $value ) . '"';
-		}
-return false;
-}
-
-function fix_no_orders_found_bug_for_woocommerce( $query_args ) {
-  if ( isset( $query_args['post_status'] ) && empty( $query_args['post_status'] ) ) {
-    unset( $query_args['post_status'] );
-  }
-  return $query_args;
-}
-//add_filter( 'request', 'fix_no_orders_found_bug_for_woocommerce', 1, 1 ); 
