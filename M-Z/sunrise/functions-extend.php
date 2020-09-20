e5782f01782a2946032fd8c610d6d24a4a383cdd
Larry <tradesouthwest@outlook.com>
2020-09-20 12:26:11 -0700

    Create functions-extend.php

---
+++ M-Z/sunrise/functions-extend.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Add MIME type to Wordpress uploader
+ * `define('ALLOW_UNFILTERED_UPLOADS', true)` Can leave vulnerabilities.
+ */
+// multiple types
+function my_myme_types($mime_types){
+    $mime_types['svg'] = 'image/svg+xml'; //Adding svg extension
+    $mime_types['psd'] = 'image/vnd.adobe.photoshop'; //Adding photoshop files
+    $existing_mimes['epub'] = 'application/epub+zip';
+    $existing_mimes['mobi'] = 'application/x-mobipocket-ebook';
+    
+      return $mime_types;
+}
+add_filter('upload_mimes', 'my_myme_types', 1, 1);
+
+// single type
+function my_theme_custom_upload_mimes( $existing_mimes ) {
+    // Add webm to the list of mime types.
+    $existing_mimes['webm'] = 'video/webm';
+
+    // Return the array back to the function with our added mime type.
+    return $existing_mimes;
+}
+add_filter( 'mime_types', 'my_theme_custom_upload_mimes' );
+
+// alternate and disallow
+function edit_upload_types($existing_mimes = array()) {
+    // allow .woff
+    $existing_mimes['woff'] = 'font/woff';
+ 
+    // add as many as you want with the same syntax
+ 
+    // disallow .jpg files
+    unset( $existing_mimes['jpg'] );
+ 
+    return $existing_mimes;
+}
+add_filter('upload_mimes', 'edit_upload_types');
+
+/**
+ * Output Mime types on a page
+ */
+function my_theme_output_upload_mimes() {
+    var_dump( wp_get_mime_types() );
+}
+add_action( 'template_redirect', 'my_theme_output_upload_mimes' );
