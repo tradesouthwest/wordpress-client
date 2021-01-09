52175cb267e1c023f381e80e8f55e8ab9c9af61a
Larry <tradesouthwest@outlook.com>
2021-01-09 01:15:48 -0700

    Update edd-license-prices.js

--- A-L/filmabontio/js/edd-license-prices.js
+++ A-L/filmabontio/js/edd-license-prices.js
@@ -1,12 +1,40 @@
-/* requires cookie.js https://github.com/UnSstrennen/cookie.js */
-
-
-var cValue = '';
-var expire = (new Date(Date.now()+ 86400*1000)).toUTCString();
-/*
- * setCookie('elp_license_prices', cValue, {expires: expire });  // string cookie
- * setCookie('elp_license_prices', {key: cValue}, {expires: expire });  // json cookie
- * getCookie('name')  // string cookies
- * getCookie('name', true)  // json cookie, returns json object
- * deleteCookie('name');  // json or string cookie: no matter!
+/* @since v. 1.0.51  
+ jQuery.post( url [, data ] [, success ] [, dataType ] ) 
+ Data(key-value pairs) 
+ Success(result,status,xhr)
+ Type type of data returned from the AJAX request (xml, json, script, or html)
  */
+jQuery(document).ready(function ($) 
+{
+
+    $('input.edd_license_prices').click(function() {
+
+        var elp_price = $('.edd_license_prices:checked').val();
+
+        $.ajax({
+            
+            url: license_upgrades.admin_ajax,
+           
+            type: 'GET',
+            var data = {
+            action: 'license_upgraded'
+            value: elp_price,
+            security:  license_upgrades.elpnonce,
+            post_data: $( form.edd_purchase_form ).serialize()
+            },
+            $.post( license_upgrades.admin_ajax, data, function( response ){
+               $( 'body' ).trigger( 'edd_add_to_cart' );
+            });
+               success: function(response) {
+                if(response) {
+                //get permalink for post from php and go to it
+                
+                console.log("set as " + elp_price);
+                }
+            }
+        });
+               
+ 
+    });
+    //return false;
+});
