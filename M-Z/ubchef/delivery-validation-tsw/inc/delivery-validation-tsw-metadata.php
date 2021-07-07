625800477855d0f4522302883815890c523ba197
Larry <tradesouthwest@gmail.com>
2021-07-07 16:45:15 -0700

    Update delivery-validation-tsw-metadata.php

--- M-Z/ubchef/delivery-validation-tsw/inc/delivery-validation-tsw-metadata.php
+++ M-Z/ubchef/delivery-validation-tsw/inc/delivery-validation-tsw-metadata.php
@@ -11,105 +11,47 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 /**
- * On Click of ‘Add To Cart’ button make an ajax call to store the custom entered data in session
+ * Add data to WooCommerce data 
  * @since 1.0.1
- */
-//add_action('wp_ajax_delivery_validation_tsw_data_options', 'delivery_validation_tsw_data_options');
-//add_action('wp_ajax_nopriv_delivery_validation_tsw_data_options', 'delivery_validation_tsw_data_options');
-add_filter( 'woocommerce_add_cart_item_data', 'delivery_validation_tsw_text_to_cart_item', 10, 3 );
-/**
- * Add engraving text to cart item.
  *
- * @param array $cart_item_data
- * @param int   $product_id
- * @param int   $variation_id
- *
- * @return array
+ * @param array Values of text field in product meta
  */
-function delivery_validation_tsw_text_to_cart_item( $cart_item_data, $product_id, $variation_id=false ) {
-	$product_menu = empty($_POST['product_menu']) ? 0 
-                    : sanitize_text_field($_POST['product_menu']);
-
-	if ( !$product_menu > 0 ) {
-		return $cart_item_data;
-	}
-
-	$cart_item_data['product_menu'] = $product_menu;
-
-	return $cart_item_data;
+function delivery_validation_tsw_add_text_input() 
+{
+    woocommerce_wp_text_input([
+        'id'        => 'delivery_validation_tsw', 
+        'label'      => 'Delivery Date Validation', 
+        'placeholder' => 'Date MUST be formatted', 
+        'wrapper_class' => 'show_if_simple',
+        'class'         => 'short',
+        'type'          => 'text',
+        'desc_tip'      => false,
+        'description' => __('Example 23/24 July would be entered as 23/07/2021. Leave empty for weekly or not dated.','woocommerce'),
+    ]);
 }
+add_action( 'woocommerce_product_options_general_product_data', 'delivery_validation_tsw_add_text_input' );
 
 /**
- * Display engraving text in the cart.
+ * Add data to WooCommerce data 
+ * @since 1.0.1
  *
- * @param array $item_data
- * @param array $cart_item
- *
- * @return array
+ * @param string $text Value of text field in product meta
  */
-function delivery_validation_tsw_display_engraving_text_cart( $item_data, $cart_item ) {
-	if ( empty( $cart_item['product_menu'] ) ) {
-		return $item_data;
-	}
+function delivery_validation_tsw_save_validated_text( $post_id ) 
+{
+    global $post;
 
-	$item_data[] = array(
-		'key'     => __( 'Delivery date', 'delivery-validation-tsw' ),
-		'value'   => wc_clean( $cart_item['product_menu'] ),
-		'display' => '',
-	);
-
-	return $item_data;
+    if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+        return;
+    
+    if ( ! current_user_can( 'edit_post', $post_id ) )
+        return;
+    
+    $post_id = $post->ID;
+    $product = wc_get_product( $post_id );
+    $text    = isset( $_POST['delivery_validation_tsw'] ) 
+                    ? $_POST['delivery_validation_tsw'] : '';
+    $product->update_meta_data( 'delivery_validation_tsw', sanitize_text_field($text));
+    $product->save();
 }
-
-add_filter( 'woocommerce_get_item_data', 'delivery_validation_tsw_display_engraving_text_cart', 10, 2 );
-
-/**
- * Add engraving text to order.
- *
- * @param WC_Order_Item_Product $item
- * @param string                $cart_item_key
- * @param array                 $values
- * @param WC_Order              $order
- */
-function delivery_validation_tsw_text_to_order_items( $item, $cart_item_key, $values, $order ) {
-	if ( empty( $values['product_menu'] ) ) {
-		return;
-	}
-
-	$item->add_meta_data( __( 'Delivery date', 'delivery-validation-tsw' ), $values['product_menu'] );
-}
-
-add_action( 'woocommerce_checkout_create_order_line_item', 'delivery_validation_tsw_text_to_order_items', 10, 4 ); 
-
-//add_action('woocommerce_ajax_added_to_cart', 'delivery_validation_tsw_fdoe_ajax', $product_id);
-//add_action('woocommerce_update_cart_action_cart_updated');
-function delivery_validation_tsw_fdoe_ajax($product_id='')
-{ 
-$product_menu = empty($_POST['product_menu']) ? 0 
-                    : sanitize_text_field($_POST['product_menu']);
-ob_start();
-if ($product_menu > 0) {
-
-    $hash = WC()->cart->add_to_cart($product_id, $product_menu );
-    $passed_vali = true;
-    $product_quantity_new = absint(1);
-    $status = false;
-            if (false != $hash) {
-                do_action('woocommerce_ajax_added_to_cart', $product_id);
-                do_action('woocommerce_update_cart_action_cart_updated');
-
-                $data = array(
-                    'success' => true,
-                    'overstock' => isset( $overstock) ? $overstock:false,
-                    'is_sold_indi'=> isset($is_sold_indi ) ? $is_sold_indi :false,
-                    'status' => $status,
-                    'product_quantity' => $product_quantity_new,
-                    'passed_vali' => true
-
-                );
-            }
-            $alert = ob_get_clean();
-    $data1 = array_merge($data, array('alert' => $alert));
-    wp_send_json($data1);
-    }
-}
+add_action( 'woocommerce_process_product_meta', 'delivery_validation_tsw_save_validated_text');
