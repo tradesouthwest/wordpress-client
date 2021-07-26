3d7e6dd5ccdf79e8805f982b5b6a1ff524e9e92c
Larry <tradesouthwest@gmail.com>
2021-07-26 12:32:07 -0700

    Update booking-validation-plugin.js
    corrected checkbox toggle state

--- M-Z/vacastay/booking-validation-tsw/js/booking-validation-plugin.js
+++ M-Z/vacastay/booking-validation-tsw/js/booking-validation-plugin.js
@@ -3,19 +3,33 @@
  */
 (function($){
 	$(document).ready(function(){
-	$('#tsw-agreeto').change(function(e){
-	    e.preventDefault();
-	    
-	    var totalcost = $('.total-costs').data("price");
-        var totalfees = $('#totalfees').val();
-        var total_cost = Number( totalcost * 1 )+Number( totalfees * 1 );
-	    
+    	$('#tsw-agreeto').click(function(){
+    	    
+    	    var chk = $(this);
+    	    chk.attr("checked", !chk.attr("checked"));
+    	    
+    	    var totalcost = $('.total-costs').data("price");
+            var totalfees = $('#totalfees').val();
+            
+            var total_cost = Number( totalcost * 1 )+Number( totalfees * 1 );
+            var reset_cost = Number( totalcost * 1 )-Number( totalfees * 1 );
+            
+    	    if( chk.attr( "checked" ) ) {
+    	        $(chk).attr('checked', true);
+    	        $('.total-costs').data("price", total_cost);
+                $('.total-costs span').html( '$'+total_cost);
 
-        
-            console.log( "added fees= " + totalfees + ' ' + totalcost );
-            $('.total-costs').data("price", total_cost);
-            $('.total-costs span').html(total_cost);
-            totalcost = totalfees = total_cost = null;
-	});
-	}); return false;
+                    console.log( "a= " + totalcost + ' ' + totalfees );    
+                    totalcost = 0;
+    	    } 
+    	    else if( !chk.attr( "checked" ) ) {
+                $(chk).attr('checked', false);
+                $('.total-costs').data("price", reset_cost);
+                $('.total-costs span').html( '$'+reset_cost);
+
+                    console.log( "b= " + totalcost + ' ' + totalfees );    
+    	    }
+        });
+    
+	}); //return false;
 })(jQuery); 
