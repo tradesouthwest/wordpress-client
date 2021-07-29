ba77edf8b3268b00f82aedf27ef1732ac3128dca
Larry <tradesouthwest@gmail.com>
2021-07-29 10:55:41 -0700

    Create booking.php

---
+++ M-Z/vacastay/booking-validation-tsw/lib/plugins/listeo-core/templates/booking.php
@@ -0,0 +1,5 @@
+// added to line 147. Just below `<ul id="booking-confirmation-summary">`
+    
+<?php /* added by larry@codeable */ 
+if ( function_exists('booking_validation_tsw_checkout_html') ) { echo do_action('booking_validation_after_booking_summary', $data); } 
+?>
