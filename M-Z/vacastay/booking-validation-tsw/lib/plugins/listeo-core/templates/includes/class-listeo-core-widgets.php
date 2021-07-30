a35249033ad092d7aacb72467376dbe7ff0a5046
Larry <tradesouthwest@gmail.com>
2021-07-30 09:53:58 -0700

    Update class-listeo-core-widgets.php

--- M-Z/vacastay/booking-validation-tsw/lib/plugins/listeo-core/templates/includes/class-listeo-core-widgets.php
+++ M-Z/vacastay/booking-validation-tsw/lib/plugins/listeo-core/templates/includes/class-listeo-core-widgets.php
@@ -1,4 +1,9 @@
 // added at line 1191 . Just before `<div class="booking-estimated-cost" `	
 
-<?php /* **************** added by larry@codeable ******************* into class-listeo-core-widgets */
-	if( function_exists('booking_validation_tsw_extend_listeo_booking_widget') ) { do_action('booking_validation_tsw_render_listeo_booking_widget', $post_id ); } ?>
+
+	<?php /* **************** added by larry@codeable ******************* into class-listeo-core-widgets */
+	if( function_exists('booking_validation_tsw_extend_listeo_booking_widget') ) : 
+	    if ( bkvaltsw_is_widget_ready() ) {
+	        do_action('booking_validation_tsw_render_listeo_booking_widget', $post_id );  
+	    }
+	endif; ?>
